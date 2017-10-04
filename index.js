110580330104dd56eafe6f6460e13512d6e3b6cf
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-04 10:35:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const TEST = true;
+import React, { Component, PropTypes } from 'react';
+
+import _ from 'lodash';
+//import dep from './dep';
+
+const a = [1];
+const b = [2,3]
+
+const otherarray = _.concat(a, b)
+
+class RandomTest extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      //h1 = dep(),
+      h2 = 'Hi TL welcome to Custom Modules! :)'
+    } = this.props;
+
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>big change.. ok</h2>
+	<h2>{otherarray[2]}</h2>
+      </div>
+    );
+  }
+}
+
+export default RandomTest;
